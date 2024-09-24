27682f835b7a825fb0b5dae3727c45fb9f362f75
esllth <61421553+esllth@users.noreply.github.com>
2024-09-23 20:32:05 -0600

    Primer Commit del proyecto Tienda SC403

---
+++ src/test/java/SC403_tienda/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package SC403_tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
